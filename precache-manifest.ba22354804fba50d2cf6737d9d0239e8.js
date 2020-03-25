23b8fd044b23aa87ec4671cc15004f20d0f63922
Siva <siva@Sivas-MacBook-Pro.local>
2020-03-25 13:08:59 +0530

    baseb4 bug fixed

---
+++ precache-manifest.ba22354804fba50d2cf6737d9d0239e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50e9e0f79b07cf7acac44d2666fd6f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7d0ffa0073fc0acb592",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "242f2e1ae0ec193f359b",
+    "url": "/static/js/2.3cf6c5ef.chunk.js"
+  },
+  {
+    "revision": "f429cf0a280a8ad62cdb3394326a00d6",
+    "url": "/static/js/2.3cf6c5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d0ffa0073fc0acb592",
+    "url": "/static/js/main.cc5ca112.chunk.js"
+  },
+  {
+    "revision": "e583927feacc73eeea17",
+    "url": "/static/js/runtime-main.497664f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a33a64cc6c0f00922cfcc91b132a70bd.js"
+  "/precache-manifest.ba22354804fba50d2cf6737d9d0239e8.js"
 );
 
 self.addEventListener('message', (event) => {
