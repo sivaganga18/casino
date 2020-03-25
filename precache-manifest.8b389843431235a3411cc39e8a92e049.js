15ba52f5d1a1921ae9b9f89a6b3e731b8a73a26b
Siva <siva@Sivas-MacBook-Pro.local>
2020-03-25 12:41:51 +0530

    base64 bug fixed

---
+++ precache-manifest.8b389843431235a3411cc39e8a92e049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279f76c14e9dda15d6ba54c7cc8741e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44aa53bfe4c17ddc2e7b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "242f2e1ae0ec193f359b",
+    "url": "/static/js/2.3cf6c5ef.chunk.js"
+  },
+  {
+    "revision": "f429cf0a280a8ad62cdb3394326a00d6",
+    "url": "/static/js/2.3cf6c5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44aa53bfe4c17ddc2e7b",
+    "url": "/static/js/main.c20863de.chunk.js"
+  },
+  {
+    "revision": "e583927feacc73eeea17",
+    "url": "/static/js/runtime-main.497664f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
